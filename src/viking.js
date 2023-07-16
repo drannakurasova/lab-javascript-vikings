fa553efe8606d4eee0166394cc600579805da405
Anna Kurasova <drannakurasova@gmail.com>
2023-07-16 20:41:36 +0200

    lab done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor (healthPar, strengthPar) {
+ this.health = healthPar;
+ this.strength = strengthPar;
+}
+
+attack = () => {
+   return this.strength; 
+}
+
+
+
+receiveDamage = (damage) => {
+    this.health = this.health - damage;
+    // tried if (damge === this.health[i]) { delete this.health[i]} but then no damage deleted nor return nothing
+    }
+  } 
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier  {
+    constructor (namePar, healthPar, strengthPar) {
+     super (namePar);
+        this.name = namePar;
+        this.health = healthPar;
+        this.strength = strengthPar;
+    }
+
+    receiveDamage = (damage) => {
+        this.health = this.health - damage;
+        if (this.health>0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }
+    } 
+
+    battleCry = () => { 
+        return `Odin Owns You All!`
+     }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon  extends Soldier {
+    receiveDamage = (damage) => {
+        this.health = this.health - damage;
+        if (this.health>0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else {
+            return `A Saxon has died in combat`
+        }
+    }
+  }   
+
 
 // War
 class War {}
